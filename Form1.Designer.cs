0ace72a58d0b089d116fd6288c8a3862b81a5b1f
Dmitry Brant <dbrant@wikimedia.org>
2016-07-17 14:23:53 -0400

    First commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Transparentizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.udBoost = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.udBoost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Transparentizer.Properties.Resources.img_placeholder;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 90);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(580, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "File";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // udBoost
+            // 
+            this.udBoost.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.udBoost.Increment = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.udBoost.Location = new System.Drawing.Point(512, 27);
+            this.udBoost.Name = "udBoost";
+            this.udBoost.Size = new System.Drawing.Size(64, 20);
+            this.udBoost.TabIndex = 2;
+            this.udBoost.ValueChanged += new System.EventHandler(this.udBoost_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(450, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Boost:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImage = global::Transparentizer.Properties.Resources.img_placeholder;
+            this.ClientSize = new System.Drawing.Size(580, 459);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.udBoost);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.udBoost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown udBoost;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+/*
+Copyright 2016 Dmitry Brant
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+namespace Transparentizer
+{
+    public partial class Form1 : Form
+    {
+        private string currentFileName;
+        private int boostAmount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = Application.ProductName;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true) { e.Effect = DragDropEffects.All; }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 0) { return; }
+            openFile(files[0]);
+        }
+
+        private void openFile(string fileName)
+        {
+            currentFileName = fileName;
+            processCurrentFile();
+        }
+
+        private void processCurrentFile()
+        {
+            try
+            {
+                Bitmap origBmp = new Bitmap(currentFileName);
+                Bitmap bmp = new Bitmap(origBmp.Width, origBmp.Height, PixelFormat.Format32bppArgb);
+
+                var g = Graphics.FromImage(bmp);
+                Console.WriteLine("Original image dimensions: " + origBmp.Width + " x " + origBmp.Height);
+                g.DrawImage(origBmp, 0, 0, origBmp.Width, origBmp.Height);
+                origBmp.Dispose();
+
+                byte[] bitmapBits = new byte[bmp.Width * bmp.Height * 4];
+                BitmapData bmpBits = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+                System.Runtime.InteropServices.Marshal.Copy(bmpBits.Scan0, bitmapBits, 0, bitmapBits.Length);
+
+                int a, h, i = 0;
+                while (i < bitmapBits.Length)
+                {
+                    h = 0;
+                    if (bitmapBits[i] > h) { h = bitmapBits[i]; }
+                    if (bitmapBits[i + 1] > h) { h = bitmapBits[i + 1]; }
+                    if (bitmapBits[i + 2] > h) { h = bitmapBits[i + 2]; }
+                    a = h - boostAmount;
+                    if (a < 0) { a = 0; }
+                    bitmapBits[i + 3] = (byte)a;
+                    i += 4;
+                }
+
+                System.Runtime.InteropServices.Marshal.Copy(bitmapBits, 0, bmpBits.Scan0, bitmapBits.Length);
+                bmp.UnlockBits(bmpBits);
+
+                pictureBox1.Width = bmp.Width;
+                pictureBox1.Height = bmp.Height;
+                pictureBox1.Image = bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void udBoost_ValueChanged(object sender, EventArgs e)
+        {
+            boostAmount = (int) udBoost.Value;
+            processCurrentFile();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var saveDlg = new SaveFileDialog();
+                saveDlg.DefaultExt = ".png";
+                saveDlg.OverwritePrompt = true;
+                saveDlg.Title = "Save";
+                saveDlg.InitialDirectory = Path.GetDirectoryName(currentFileName);
+                saveDlg.FileName = Path.GetFileNameWithoutExtension(currentFileName) + ".png";
+                if (saveDlg.ShowDialog() == DialogResult.Cancel) return;
+
+                pictureBox1.Image.Save(saveDlg.FileName, ImageFormat.Png);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+    }
+}
